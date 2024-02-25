58d94fabae5faf44c3bae1f3a369d8426fb6052d
jrodez <jeremie.rodez@irit.fr>
2024-02-25 01:52:32 +0100

    upgrade nixpkgs to f7e8132

--- default.nix
+++ default.nix
@@ -1,8 +1,9 @@
 # If called without explicitly setting the 'pkgs' arg, a pinned nixpkgs version is used by default.
-{ pkgs ? import (fetchTarball {
-    url = "https://github.com/NixOS/nixpkgs/archive/23.11.tar.gz";
-    sha256 = "sha256:1ndiv385w1qyb3b18vw13991fzb9wg4cl21wglk89grsfsnra41k";
-  }) {}
+{ pkgs ? (import (fetchTarball {
+    name = "nixpkgs-f7e8132"; # 2024-02-??
+    url = "https://github.com/NixOS/nixpkgs/archive/f7e8132daca31b1e3859ac0fb49741754375ac3d.tar.gz";
+    sha256 = "0z57px1wqjm9z51ifc9b0fccp0a9w9sakm9c1ybvkib096gmlp0g";
+  })) { }
 , debug ? false
 }:
 
@@ -22,4 +23,4 @@ let
     simgrid-light-master = pkgs.callPackage ./pkgs/simgrid/master.nix { simgrid = nur-pkgs.simgrid-light; };
   };
 in
-  nur-pkgs // master-pkgs
+nur-pkgs // master-pkgs
