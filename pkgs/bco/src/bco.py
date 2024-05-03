62e7ad45603fcb1cc1bcd09b246c7e2e2b62241b
jrodez <jeremie.rodez@irit.fr>
2024-05-03 15:16:35 +0200

    add BCO

--- pkgs/bco/src/bco.py
+++ pkgs/bco/src/bco.py
@@ -119,19 +119,20 @@ if __name__ == "__main__":
     print("", end="", flush=True)
     print("\n", end="", flush=True)
 
-    print(f"{color.BOLD}Resume{color.END}")
+    print(f"{color.BOLD}Compilation report summary :{color.END}")
+    print("  │")
     if len(warnings) > 0:
-        print(f"├─{color.YELLOW}{color.BOLD}Warnings{color.END}")
+        print(f"  ├──{color.YELLOW}{color.BOLD}Warnings :{color.END}")
         for i, warning in enumerate(warnings):
             print(
-                f"│  {'├'if i+1 < len(warnings) else '└'}─ " + warning.strip(), end="\n"
+                f"  │   {'├'if i+1 < len(warnings) else '└'}─ " + warning.strip(), end="\n"
             )
     else:
-        print(f"├─{color.GREEN}No warning found.{color.END}")
-
+        print(f"  ├──{color.GREEN}No warning found.{color.END}")
+    print("  │")
     if len(errors) > 0:
-        print(f"└─{color.RED}{color.BOLD}Errors{color.END}")
+        print(f"  └──{color.RED}{color.BOLD}Errors :{color.END}")
         for i, error in enumerate(errors):
-            print(f"   {'├'if i+1 < len(errors) else '└'}─ " + error.strip(), end="\n")
+            print(f"      {'├'if i+1 < len(errors) else '└'}─ " + error.strip(), end="\n")
     else:
-        print(f"└─{color.GREEN}No error found.{color.END}")
+        print(f"  └──{color.GREEN}No error found.{color.END}")
