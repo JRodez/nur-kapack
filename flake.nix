7f82ba7ac53da9a4207a57960936e93c7f5fb096
jrodez <jeremierodez@outlook.com>
2024-12-13 13:32:18 +0100

    unstable

--- flake.nix
+++ flake.nix
@@ -2,7 +2,7 @@
   description = " My personal NUR repository";
   inputs = {
     flake-utils.url = "github:numtide/flake-utils";
-    nixpkgs.url = "github:nixos/nixpkgs";
+    nixpkgs.url = "github:nixos/nixpkgs/nixos-unstable";
   };
 
   outputs = { self, nixpkgs, flake-utils }:
