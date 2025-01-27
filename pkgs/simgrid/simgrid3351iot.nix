2fb49329392217696a264bc86e412af3fb9679b8
jrodez <jeremierodez@outlook.com>
2025-01-27 21:08:19 +0100

    fix

--- pkgs/simgrid/simgrid3351iot.nix
+++ pkgs/simgrid/simgrid3351iot.nix
@@ -83,9 +83,9 @@ stdenv.mkDerivation rec {
   doCheck = true;
   preCheck = ''
     # prevent the execution of tests known to fail
-    # cat <<EOW >CTestCustom.cmake
-    # SET(CTEST_CUSTOM_TESTS_IGNORE smpi-replay-multiple)
-    # EOW
+    cat <<EOW >CTestCustom.cmake
+    SET(CTEST_CUSTOM_TESTS_IGNORE smpi-replay-multiple)
+    EOW
 
     # make sure tests are built in parallel (this can be long otherwise)
     make tests -j $NIX_BUILD_CORES
