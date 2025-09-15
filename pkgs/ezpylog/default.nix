3f86c2b86d6b4f26a8f2b8679405fcc6aef9896d
jrodez <jeremierodez@outlook.com>
2025-09-15 17:11:54 +0200

    update ezpylog

--- pkgs/ezpylog/default.nix
+++ pkgs/ezpylog/default.nix
@@ -1,4 +1,8 @@
-{lib, python3Packages, fetchFromGitHub}:
+{
+  lib,
+  python3Packages,
+  fetchFromGitHub,
+}:
 python3Packages.buildPythonPackage rec {
   pname = "ezpylog";
   version = "2.2.0";
@@ -12,6 +16,11 @@ python3Packages.buildPythonPackage rec {
     sha256 = "sha256-4PaHbK1QY/eoZ8ZIhQJ8caaE914pmbm6JXYIiUeI1Ro=";
   };
 
+  pyproject = true;
+  build-system = with python3Packages; [
+    setuptools
+  ];
+
   propagatedBuildInputs = with python3Packages; [
 
   ];
@@ -21,11 +30,11 @@ python3Packages.buildPythonPackage rec {
 
   meta = with lib; {
     description = "ezpylog is a minimalistic and easy to use python logger";
-    homepage    = https://github.com/JRodez/ezpylog;
-    platforms   = python3Packages.debugpy.meta.platforms; # TODO: be more precise if possible
+    homepage = "https://github.com/JRodez/ezpylog";
+    platforms = python3Packages.debugpy.meta.platforms; # TODO: be more precise if possible
     license = licenses.gpl3;
     longDescription = ''
-        ezpylog is a minimalistic and easy to use python logger
+      ezpylog is a minimalistic and easy to use python logger
     '';
     broken = false;
   };
